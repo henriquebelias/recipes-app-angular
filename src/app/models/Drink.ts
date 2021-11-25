463ae6829830b4f1c5e91be16723079c81619d2c
Henrique Brito Elias <henriquebelias@gmail.com>
2021-11-25 11:33:00 -0300

    recipe details page draft completed

--- src/app/models/Drink.ts
+++ src/app/models/Drink.ts
@@ -8,6 +8,7 @@ export interface Drink {
   strGlass: string,
   strInstructions: string,
   strDrinkThumb: string,
+  strCategory: string,
   strIngredient1?: string,
   strIngredient2?: string,
   strIngredient3?: string,
